1769a70c28fdd7c5b18e2b5feacb6ca6291d4cb8
TheSpace-hub <78466469+TheSpace-hub@users.noreply.github.com>
2025-08-21 19:06:14 +0300

    Загрузка конфига

--- src/main/java/hub/thespace/xorekitemstake/XorekItemsTake.java
+++ src/main/java/hub/thespace/xorekitemstake/XorekItemsTake.java
@@ -1,12 +1,16 @@
 package hub.thespace.xorekitemstake;
 
+import hub.thespace.xorekitemstake.config.ConfigManager;
 import hub.thespace.xorekitemstake.executor.MainExecutor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class XorekItemsTake extends JavaPlugin {
 
+    private final ConfigManager configManager = new ConfigManager(this);
+
     @Override
     public void onEnable() {
+        configManager.loadConfig();
         getServer().getPluginManager().registerEvents(new MainExecutor(this), this);
     }
 }
