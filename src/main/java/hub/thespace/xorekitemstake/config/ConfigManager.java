8666efd5c2c644a8b5311a0431a5b18e1b8c0120
TheSpace-hub <78466469+TheSpace-hub@users.noreply.github.com>
2025-08-21 19:09:16 +0300

    Безопасное получение строки из конфига

--- src/main/java/hub/thespace/xorekitemstake/config/ConfigManager.java
+++ src/main/java/hub/thespace/xorekitemstake/config/ConfigManager.java
@@ -1,5 +1,6 @@
 package hub.thespace.xorekitemstake.config;
 
+import org.bukkit.ChatColor;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.Plugin;
 
@@ -21,6 +22,20 @@ public class ConfigManager {
         plugin.saveDefaultConfig();
         plugin.reloadConfig();
         config = plugin.getConfig();
+    }
+
+    public String getString(String path, String defaultValue) {
+        try {
+            String value = config.getString(path);
+            if (value == null || value.trim().isEmpty()) {
+                plugin.getLogger().warning("Значение для пути '" + path + "' не найдено или пустое. Используется значение по умолчанию: " + defaultValue);
+                return defaultValue;
+            }
+            return ChatColor.translateAlternateColorCodes('&', value);
+        } catch (Exception e) {
+            plugin.getLogger().warning("Ошибка при чтении строки из конфига по пути: " + path);
+            return defaultValue;
+        }
     }
 
 }
