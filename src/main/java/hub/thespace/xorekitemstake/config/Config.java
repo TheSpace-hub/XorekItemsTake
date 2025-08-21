8a8ff41c9e9192d65371f10fe532d1769c21c4bd
TheSpace-hub <78466469+TheSpace-hub@users.noreply.github.com>
2025-08-21 19:13:28 +0300

    Создал класс получения данных из конфига.

---
+++ src/main/java/hub/thespace/xorekitemstake/config/Config.java
@@ -0,0 +1,13 @@
+package hub.thespace.xorekitemstake.config;
+
+/**
+ * Получение данных из конфига.
+ */
+public class Config {
+    private final ConfigManager configManager;
+
+    public Config(ConfigManager configManager) {
+        this.configManager = configManager;
+    }
+
+}
