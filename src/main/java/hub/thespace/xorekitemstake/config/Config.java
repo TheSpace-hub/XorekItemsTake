a217b28b52825ca7467d591a73b2ac86aff846b8
TheSpace-hub <78466469+TheSpace-hub@users.noreply.github.com>
2025-08-21 18:55:35 +0300

    Получение запрещающего сообщения

---
+++ src/main/java/hub/thespace/xorekitemstake/config/Config.java
@@ -0,0 +1,12 @@
+package hub.thespace.xorekitemstake.config;
+
+public class Config {
+    private String prefix;
+    private String messages;
+    private String bossbar;
+
+    public static class Mode {
+
+    }
+
+}
