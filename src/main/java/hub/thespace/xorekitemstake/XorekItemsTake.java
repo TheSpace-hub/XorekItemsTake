87b2a624fabfe9a83a44b6b1628290b0a5ff5713
TheSpace-hub <78466469+TheSpace-hub@users.noreply.github.com>
2025-08-22 10:26:37 +0300

    Запуск каждую 1/2 часть секунды

--- src/main/java/hub/thespace/xorekitemstake/XorekItemsTake.java
+++ src/main/java/hub/thespace/xorekitemstake/XorekItemsTake.java
@@ -1,12 +1,16 @@
 package hub.thespace.xorekitemstake;
 
 import hub.thespace.xorekitemstake.executor.MainExecutor;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class XorekItemsTake extends JavaPlugin {
 
+    private final MainExecutor mainExecutor = new MainExecutor(this);
+
     @Override
     public void onEnable() {
-        getServer().getPluginManager().registerEvents(new MainExecutor(this), this);
+        getServer().getPluginManager().registerEvents(mainExecutor, this);
+        Bukkit.getScheduler().runTaskTimer(this, mainExecutor, 10L, 10L);
     }
 }
