35a1326fb0dd124510986308f9799268eb862e3a
TheSpace-hub <78466469+TheSpace-hub@users.noreply.github.com>
2025-08-22 10:09:59 +0300

    Сообщение о блокировке не спамится в чат

--- src/main/java/hub/thespace/xorekitemstake/executor/MainExecutor.java
+++ src/main/java/hub/thespace/xorekitemstake/executor/MainExecutor.java
@@ -6,6 +6,7 @@ package hub.thespace.xorekitemstake.executor;
 import hub.thespace.xorekitemstake.RejectionMode;
 import hub.thespace.xorekitemstake.config.Config;
 import hub.thespace.xorekitemstake.config.ConfigManager;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -13,13 +14,19 @@ import org.bukkit.event.entity.EntityPickupItemEvent;
 import org.bukkit.plugin.Plugin;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Обработчик поднятия игроком предмета.
  */
-public class MainExecutor implements Listener {
+public class MainExecutor implements Listener, Runnable {
 
     private final Plugin plugin;
     private final Config config;
+    private final Map<Player, Integer> notifiedPlayers = new HashMap<>();
 
     public MainExecutor(Plugin plugin) {
         this.plugin = plugin;
@@ -30,15 +37,17 @@ public class MainExecutor implements Listener {
      * Срабатывает при поднятии предмета.
      * Проверяет что поднял игрок и у него нет прав для обхода.
      *
-     * @param event -
+     * @param event Поднятие игроком предмета с земли.
      */
     @EventHandler
     public void onEntityPickupItem(@NotNull EntityPickupItemEvent event) {
-        if (!(event.getEntity() instanceof Player player) || player.hasPermission("xorekItemsTake.bypass"))
+        if (!(event.getEntity() instanceof Player player)
+                || player.hasPermission("xorekItemsTake.bypass"))
             return;
 
-        if (canPickup(player)) {
+        if (!canPickup(player)) {
             sendRejectionMessage(player);
+            notifiedPlayers.put(player, Bukkit.getServer().getCurrentTick());
             event.setCancelled(true);
         }
     }
@@ -51,7 +60,7 @@ public class MainExecutor implements Listener {
      */
     private boolean canPickup(@NotNull Player player) {
         RejectionMode mode = RejectionMode.fromPlayer(player);
-        return mode != null;
+        return mode == null;
     }
 
     /**
@@ -60,6 +69,9 @@ public class MainExecutor implements Listener {
      * @param player Игрок, который не может поднять предмет.
      */
     private void sendRejectionMessage(Player player) {
+        if (notifiedPlayers.containsKey(player))
+            return;
+
         RejectionMode mode = RejectionMode.fromPlayer(player);
         assert mode != null;
 
@@ -74,5 +86,20 @@ public class MainExecutor implements Listener {
                 .replace("{prefix}", config.getPrefix())
                 .replace("{mode}", placeholder)
         ));
+    }
+
+    /**
+     * Удаляет игроков из оповещённых, если они были оповещены более 1-ого тика назад.
+     */
+    @Override
+    public void run() {
+        List<Player> noLongerNotifiedPlayers = new ArrayList<>();
+        for (Player player : notifiedPlayers.keySet()) {
+            if (notifiedPlayers.get(player) < Bukkit.getServer().getCurrentTick() - 20)
+                noLongerNotifiedPlayers.add(player);
+        }
+        for (Player player : noLongerNotifiedPlayers) {
+            notifiedPlayers.remove(player);
+        }
     }
 }
